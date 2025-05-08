53aea23af5005b3311e300fb716fc1b687384bf5
Jesús Alós <alosjesus13@gmail.com>
2025-05-08 09:34:10 +0000

    Api Estática

--- src/app.py
+++ src/app.py
@@ -29,13 +29,45 @@ def sitemap():
     return generate_sitemap(app)
 
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+@app.route("/members", methods=["GET"])
+def get_all_members():
+        members = jackson_family.get_all_members()
+        return jsonify(members), 200
+    
+
+@app.route("/members/<int:member_id>", methods=["GET"])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+@app.route("/members", methods=["POST"])
+def add_member():
+    request_body = request.get_json()
+    if request_body is None:
+        raise APIException("You need to specify the request body as a json", status_code=400)
+    member = {
+        "first_name": request_body.get("first_name"),
+        "last_name": "Jackson",
+        "age": request_body.get("age"),
+        "lucky_numbers": request_body.get("lucky_numbers")
+    }
+    if all(key in member for key in ("first_name", "age", "lucky_numbers")):
+        jackson_family.add_member(member)
+        new_member = jackson_family.get_all_members()[-1]
+        return jsonify(new_member), 200 
+    else:
+        return jsonify({"msg": "Invalid member data"}), 400
+
+@app.route("/members/<int:member_id>", methods=["DELETE"])
+def delete_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if not member:
+        return jsonify({"error": "Member not found"}), 404
+    jackson_family.delete_member(member_id)
+    return jsonify({"done": True}), 200
 
 
 
